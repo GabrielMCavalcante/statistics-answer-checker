07b5aa441bc78bad6f0cb2a78d924c0bbeb43a87
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-27 13:55:38 -0300

    Created Data component

---
+++ src/components/Data/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// CSS styles
+import './styles.css'
+
+function Data() {
+
+    return (
+        <div className="Data">
+            <textarea required placeholder="Type or paste data here..."></textarea>
+        </div>
+    )
+}
+
+export default Data
