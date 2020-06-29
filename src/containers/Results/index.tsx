c5c7e4e5cf9b824aa7ffe095da10a5ef250fe0e3
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-29 10:23:02 -0300

    Added Option2 route

--- src/containers/Results/index.tsx
+++ src/containers/Results/index.tsx
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 
 // Components
 import Option1 from 'components/Option1'
+import Option2 from 'components/Option2'
 
 // Containers
 import ResultOptions from 'containers/ResultOptions'
@@ -17,8 +18,8 @@ function Results(props: any) {
             <div className="ResultScreen">
                 <Switch>
                     <Route path="/results/1" component={Option1} />
-                    {/* <Route path="/results/2" component={Option2} />
-                    <Route path="/results/3" component={Option3} />
+                    <Route path="/results/2" component={Option2} />
+                    {/* <Route path="/results/3" component={Option3} />
                     <Route path="/results/4" component={Option4} /> */}
                     {/* <Redirect to="/results/1" /> */}
                 </Switch>
