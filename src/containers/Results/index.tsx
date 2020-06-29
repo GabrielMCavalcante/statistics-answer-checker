f564fd5ac0943272239b3f38c7be7ec43fc4b028
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-29 12:40:53 -0300

    Refactored to use Option component

--- src/containers/Results/index.tsx
+++ src/containers/Results/index.tsx
@@ -1,28 +1,58 @@
-import React from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
 // Components
-import Option1 from 'components/Option1'
-import Option2 from 'components/Option2'
-import Option3 from 'components/Option3'
-import Option4 from 'components/Option4'
+import Option from 'components/Option'
 
 // Containers
 import ResultOptions from 'containers/ResultOptions'
+import MainTable from 'containers/MainTable'
+import DataSetTable from 'containers/DataSetTable'
+import FrequencyTable from 'containers/FrequencyTable'
+import ExtraInfoTable from 'containers/ExtraInfoTable'
 
 // CSS styles
 import './styles.css'
 
+const options: JSX.Element[] = []
+
+const optionsConfig = [
+    { title: 'Generated Table', content: <MainTable /> },
+    { title: 'Data Set', content: <DataSetTable /> },
+    { title: 'Absolute Frequency', content: <FrequencyTable /> },
+    { title: 'Extra Information', content: <ExtraInfoTable /> }
+]
+
 function Results(props: any) {
+
+    const goBack = useCallback(() => {
+        sessionStorage.removeItem('jsonTable')
+        props.history.push('/')
+    }, []) // eslint-disable-line
+
+    // useEffect(() => {
+    //     if (!sessionStorage.getItem('jsonTable')) goBack()
+    // }, [goBack])
+
+    optionsConfig.forEach(option => {
+        options.push((
+            <Option
+                title={option.title}
+                content={option.content}
+                returnToMenu={goBack}
+            />
+        ))
+    })
+
     return (
         <div className="Results">
-            <ResultOptions history={props.history}/>
+            <ResultOptions history={props.history} />
             <div className="ResultScreen">
                 <Switch>
-                    <Route path="/results/1" component={Option1} />
-                    <Route path="/results/2" component={Option2} />
-                    <Route path="/results/3" component={Option3} />
-                    <Route path="/results/4" component={Option4} />
+                    <Route path="/results/1" render={() => options[0]} />
+                    <Route path="/results/2" render={() => options[1]} />
+                    <Route path="/results/3" render={() => options[2]} />
+                    <Route path="/results/4" render={() => options[3]} />
                     <Redirect to="/results/1" />
                 </Switch>
             </div>
