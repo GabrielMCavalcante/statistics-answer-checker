e0be859145772b98709d4d34887585a35f398096
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-30 09:04:08 -0300

    Created interface for generated data

---
+++ src/containers/Home/GeneratedData/interface.ts
@@ -0,0 +1,24 @@
+export default interface GeneratedData {
+    table: number[]
+    unique_values: number[]
+    sturges_k: number
+    class_interval: number
+    intervals: number[][]
+    medium_value: number[]
+    interval_freq: number[]
+    accumulated_freq: number[]
+    absolute_freq: number[]
+    total_values: number
+    relative_freq: number[]
+    accumulated_relative_freq: number[]
+    total_amplitude: number
+    amostral_amplitude: number
+    variance: number
+    standard_deviation: number
+    variation_coefficient: number
+    lines: number
+    columns: number
+    average: number
+    fashion: number[]
+    median: number
+}
