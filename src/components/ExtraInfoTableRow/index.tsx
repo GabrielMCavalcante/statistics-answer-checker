b5caf096e20d04c1497f38f5633e68c092973f91
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-30 15:54:47 -0300

    Added props parsing

--- src/components/ExtraInfoTableRow/index.tsx
+++ src/components/ExtraInfoTableRow/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-function ExtraInfoTableRow(props: { label: string, value: string }) {
+function ExtraInfoTableRow(props: { label: string, value: number | number[] }) {
+
+    const parsedValue = typeof props.value === 'number'
+        ? parseFloat(props.value.toFixed(2))
+        : [...props.value].map((v, i) => { 
+            return String(parseFloat(v.toFixed(2))) + ', '
+        }).join('')
+
     return (
         <tr>
-            <td>{ props.label }</td>
-            <td>{ props.value }</td>
+            <td>{props.label}</td>
+            <td>{parsedValue}</td>
         </tr>
     )
 }
