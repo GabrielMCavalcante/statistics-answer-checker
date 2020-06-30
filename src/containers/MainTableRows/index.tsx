7df32293289b0f815bb38334fe5830676d311977
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-30 09:05:26 -0300

    Added fetcher for used generated data

--- src/containers/MainTableRows/index.tsx
+++ src/containers/MainTableRows/index.tsx
@@ -3,28 +3,51 @@ import React from 'react'
 // Components
 import MainTableRow from 'components/MainTableRow'
 
-function MainTableRows() {
+type Info = {
+    classes: number,
+    interval: number[][],
+    xi: number[],
+    fi: number[],
+    fr: number[],
+    fac: number[],
+    facr: number[]
+}
 
-    const rows = [
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-        {class: 1, interval: [1, 5], xi: 3, fi: 2, fr: 4.16, fac: 2, facr: 4.16},
-    ]
+type RowData = {
+    class: number,
+    interval: number[],
+    xi: number,
+    fi: number,
+    fr: number,
+    fac: number,
+    facr: number
+}
+
+function MainTableRows(props: { info: Info }) {
+    const totClasses: number[] = []
+
+    for (let i = 1; i <= props.info.classes; i++)
+        totClasses.push(i)
+
+    const rows: RowData[] = []
+
+    for (let row = 0; row < props.info.classes; row++) {
+        rows.push({ 
+            class: totClasses[row], 
+            interval: props.info.interval[row], 
+            xi: props.info.xi[row], 
+            fi: props.info.fi[row], 
+            fr: props.info.fr[row], 
+            fac: props.info.fac[row], 
+            facr: props.info.facr[row]
+        })
+    }
 
     return (
         <>
             {
                 rows.map(row => (
-                    <MainTableRow key={rows.indexOf(row)} {...row}/>
+                    <MainTableRow key={rows.indexOf(row)} {...row} />
                 ))
             }
         </>
