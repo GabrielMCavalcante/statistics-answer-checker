84903bf5a77ba6dca0d44bf887333dcfa52f2033
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-01 12:42:54 -0300

    Fixed multivalue formatting

--- src/components/ExtraInfoTableRow/index.tsx
+++ src/components/ExtraInfoTableRow/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { formatValue } from 'utils/ValueFormatter'
 
 function ExtraInfoTableRow(props: { label: string, value: number | number[] }) {
 
+    const arr = typeof props.value === 'object' ? [...props.value] : []
+    const length = arr.length
+
     const parsedValue = typeof props.value === 'number'
-        ? parseFloat(props.value.toFixed(2))
+        ? formatValue(parseFloat(props.value.toFixed(2)))
         : [...props.value].map((v, i) => { 
-            return String(parseFloat(v.toFixed(2))) + ', '
+            return String(formatValue(parseFloat(v.toFixed(2)))) + (i !== length - 1 ? (arr[i + 2] ? ', ' : ' & ') : '')
         }).join('')
 
     return (
