696e2f1a7fc3bf4a90ff64673f492f86c3710e37
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-02 09:30:32 -0300

    Updates

---
+++ precache-manifest.cde11e39aa3c62d516bab0287a51aade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88139c86607cef1869795a93e84912d",
+    "url": "/statistics-answer-checker/index.html"
+  },
+  {
+    "revision": "26409bee44e666bad6cd",
+    "url": "/statistics-answer-checker/static/css/main.740da807.chunk.css"
+  },
+  {
+    "revision": "7471e58a975f2e8d7fce",
+    "url": "/statistics-answer-checker/static/js/2.f87d9ad0.chunk.js"
+  },
+  {
+    "revision": "dde2f98dd0ed8bfad18d88defdcf5f43",
+    "url": "/statistics-answer-checker/static/js/2.f87d9ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26409bee44e666bad6cd",
+    "url": "/statistics-answer-checker/static/js/main.a6902da5.chunk.js"
+  },
+  {
+    "revision": "08ff946cf26a7d28356b",
+    "url": "/statistics-answer-checker/static/js/runtime-main.bd3b681e.js"
+  }
+]);
